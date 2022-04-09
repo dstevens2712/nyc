975493c94db31a0cdad58c36736eee339faf9c13
David Stevens <dstevens2712@gmial.com>
2022-04-09 07:42:29 -0400

    Initiated the ActivitiesView Class and the VenueView Class

--- nyc/views.py
+++ nyc/views.py
@@ -18,9 +18,28 @@ class BoroughView(View):
         )
 
 
-class ActivityView(View):
-    pass
+class ActivityView(View):                                           
+   def get(self, request, borough, activity):
+       return render(
+           request=request,
+           template_name='activities.html',
+           context={'borough'/: borough, 'activity': activity, 
+                    'venues': boroughs[borough][activity].keys()
+                   }
+       ) # Defines the Class ActivityView, Requests the available activities from the boroughs.py file,
+         # retuns the information so it can be passed forward to the webpage.
 
 
 class VenueView(View):
-    pass
+    def get(self, request, borough, activity, venue):
+        return=render(
+            request=request,
+            template_name='venue.html',
+            context={'borough': borough,
+                     'venue': venue,
+                     'venueDesc': bouroughs[borough][activity][venue]
+                     ['description']
+                    }
+        )   # Defines the Class VenueView, Requests the venue information from the bouroughs.py file,
+            # retuns the information so it can be passed forward to the webpage.
+  
